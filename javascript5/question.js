ca89013fd267ac4ae5b37460fc4ae8db720474a4
harshdeep <hs4205170@gmail.com>
2024-03-03 12:15:04 +0530

    day7 commit

---
+++ javascript5/question.js
@@ -0,0 +1,22 @@
+let num=prompt("Enter a number");
+if(num<=100&&num>=90){
+    console.log("THE GRADE IS A");
+}
+else if(num<90&&num>=70){
+    console.log("THE GRADE IS B");
+}
+else if(num<90&&num>=70){
+    console.log("THE GRADE IS B");
+}
+else if(num<70&&num>=60){
+    console.log("THE GRADE IS C");
+}
+else if(num<60&&num>=50){
+    console.log("THE GRADE IS D");
+}
+else if(num<50&&num>=0){
+    console.log("THE GRADE IS F");
+}
+else{
+    console.log("U put the wrong input!!");
+}
