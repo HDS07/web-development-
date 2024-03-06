de25017f8e01aeceffee7cedd0715926f8a85066
harshdeep <hs4205170@gmail.com>
2024-03-06 23:20:36 +0530

    DOM push

---
+++ DOM/script.js
@@ -0,0 +1,14 @@
+// let heading=document.getElementById("heading1");
+// console.dir(heading);
+
+// let classs=document.getElementsByClassName("class1");
+// console.dir(classs);
+
+// let  tag=document.getElementsByTagName("p");
+// console.dir(tag);
+
+let querysel=document.querySelector("p");
+console.dir(querysel);
+
+// let queryselall=document.querySelectorAll("p");
+// console.dir(queryselall);
