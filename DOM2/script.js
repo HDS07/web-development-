9aba9190b2dcb5a69482ece504cc8295a4996162
harshdeep <hs4205170@gmail.com>
2024-03-08 18:39:14 +0530

    another commit

---
+++ DOM2/script.js
@@ -0,0 +1,12 @@
+let heading=document.querySelector("h1");
+console.dir(heading.innerText);
+heading.innerText=heading.innerText+" World!";
+console.dir(heading.innerText);
+
+let classs=document.querySelector("div");
+console.log(classs);
+
+let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+console.log(para.setAttribute("class","demooooooo"));
